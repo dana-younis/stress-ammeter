b892c38b9fbd157df2bacb83e9c173bdce58154b
Rula Al-qasem <jaradatrula6@gmail.com>
2021-07-13 18:17:09 +0300

    rula alqasem

--- server/server.js
+++ server/server.js
@@ -1,44 +1,49 @@
 const express = require('express');
-const app = express();
+
 const http = require('http');
 const PORT = process.env.PORT || 5000;
+const staffRoom = ""
+
+const io = require('socket.io');
 const cors = require('cors');
+const app = express();
 const server = http.createServer(app);
-const io = require('socket.io')(http);
-const staffRoom = 'staff';
-const { v4: uuidv4 } = require('uuid');
-io.listen(server);
+
+const socket = io(server, {
+  cors: {
+    origin: '*'
+  }
+})
+
+socket.on('connection', (socket) => {
+  console.log('clie.nt connected', socket.id);
+
+  socket.on('counter', (payload) => {
+    console.log(",,,", payload.counter)
+    counter = payload.counter;
+    socket.emit('result', { counter: payload.counter })
+
+  })
+
+
+  socket.on('disconnect', () => {
+    console.log(socket.id, "disconnected");
+  })
+
+
+
+
+
+
+
+
+
+
+})
+
 
 app.use(cors());
-// app.get('/hi', (req, res) => {
-//   res.send('Hello World');
-// });
 
-io.on('connection', (socket) => {
-  // console.log('clie.nt connected', socket.id);
-  //2a
-  socket.on('join', (payload) => {
-    // socket.join will put the socket in a private room
-    socket.join(staffRoom);
-    socket.to(staffRoom).emit('onlineStaff', { name: payload.name, id: socket.id, score: payload.score });
-  });
-  socket.on('createTicket', (payload) => {
-    // 2
-    socket
-      .in(staffRoom)
-      .emit('newTicket', { ...payload, id: uuidv4(), socketId: socket.id, score: payload.score });
-  });
-
-  socket.on('claim', (payload) => {
-    // 2
-    socket
-      .in(staffRoom)
-      .emit('newTicket', { ...payload, id: uuidv4(), socketId: socket.id, score: payload.score });
-  });
-  socket.on('disconnect', () => {
-    socket.to(staffRoom).emit('offlineStaff', { id: socket.id, score: payload.score });
-  });
-});
 server.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
 });
