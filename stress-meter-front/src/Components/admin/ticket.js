788cf706ff98725640dd627c5ad9c05842cd3259
Rula Al-qasem <jaradatrula6@gmail.com>
2021-07-13 18:18:50 +0300

    rula alqasem

--- stress-meter-front/src/Components/admin/ticket.js
+++
@@ -1,9 +0,0 @@
-export default function Ticket(props) {
-  return (
-    <article className="ticket">
-      <h2> bbbb {props.studentName}</h2>
-      <p>score: {props.counter}</p>
-      <button onClick={() => props.handleClaim(props.id, props.socketId, props.counter, props.studentName)}>Claim</button>
-    </article>
-  )
-}
---
+++ stress-meter-front/src/component/QCard.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+import "react-bootstrap/dist/react-bootstrap.min.js";
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
+
+let questions = [' Do you frequently neglect your diet?', 'Do you frequently try to do everything yourself?', 'Do you blow up easily and often?', 'Do you frequently seek unrealistic goals?', 'Do you find it difficult to enjoy humor and entertainment?', 'Do you frequently and easily get irritated?', 'Do you frequently seem to make a "big deal" of everything?', 'Do you frequently complain that you are disorganized?', 'Do you tend to keep everything inside?', 'Do you frequently neglect exercise?', 'Do you have few supportive relationships?', 'Do you often get too little rest?', 'Do you frequently get angry when you are kept waiting?', ' Do you often ignore symptoms of poor health?', 'Do you frequently put things off until later?', ' Do you frequently think there is only one right way to do something?', 'Do you have an absence of enjoyable activities in your life?', 'Do you frequently find yourself spending a lot of time complaining about the past?', '. Do you wake up feeling anxious about going to work?', 'Do you often feel unable to cope with all you have to do?', 'Do you care for an elderly or ill relative?']
+export default class QCard extends React.Component {
+
+
+  render() {
+    return (
+      questions.map((qu, idx) => {
+        return <Card style={{ width: '18rem' }} id={idx}>
+          <Card.Body>
+            <Card.Title>{`Question ${idx + 1}`}</Card.Title>
+            <Card.Text>
+              {`${qu} `}
+            </Card.Text>
+            <Button variant="primary" onClick={this.props.yesFunction}> yes </Button>
+            <Button variant="primary">no</Button>
+          </Card.Body>
+        </Card>
+      })
+
+
+    )
+  }
+}
