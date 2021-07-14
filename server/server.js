31f84054efa9f3fa7f9800498fa15f3960f14bbc
Rula Al-qasem <jaradatrula6@gmail.com>
2021-07-15 01:10:27 +0300

    ch

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const http = require('http');
 const PORT = process.env.PORT || 5000;
-const staffRoom = ""
-
+const userRoom = ""
+const { v4: uuidv4 } = require('uuid');
 const io = require('socket.io');
 const cors = require('cors');
 const app = express();
@@ -15,19 +15,41 @@ const socket = io(server, {
   }
 })
 
+const queue = {
+  tickets: [],
+  user: [],
+};
+app.use(cors());
 socket.on('connection', (socket) => {
   console.log('client connected', socket.id);
 
   socket.on('counter', (payload) => {
     console.log(",,,", payload.counter)
     counter = payload.counter;
+
+    const ticketData = { payload, id: uuidv4(), socketId: socket.id };
+    queue.tickets.push(ticketData);
+    socket.in(userRoom).emit('newTicket', ticketData);
+
     socket.emit('result', { counter: payload.counter })
 
+
   })
+
+  socket.on('join', (payload) => {
+    console.log('userjooooooin');
+    // socket.join will put the socket in a private room
+    console.log("join", payload);
+    const user = { name: payload.name, id: socket.id };
+    queue.user.push(user);
+    socket.join(userRoom);
+    socket.to(userRoom).emit('onlineUser', user);
+  });
 
 
   socket.on('disconnect', () => {
     console.log(socket.id, "disconnected");
+    socket.to(userRoom).emit('offlineUser', { id: socket.id });
   })
 
 
@@ -42,7 +64,7 @@ socket.on('connection', (socket) => {
 })
 
 
-app.use(cors());
+
 
 server.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
--- stress-meter-front/src/component/User.js
+++ stress-meter-front/src/component/User.js
@@ -1,11 +1,124 @@
-import React, { Component } from 'react'
+// import React from 'react'
 
-export default class User extends Component {
+
+
+
+// import io from 'socket.io-client';
+// let socket;
+// const options = {
+//   transport: ['websocket'],
+//   upgrade: false,
+// };
+// socket = io('http://localhost:5000/', options);
+
+
+
+// export default class User extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       userName: "",
+//       tickets: [],
+//       onlineuser: [],
+//     }
+//   }
+//   componentDidMount() {
+//     // run once when component is mounted
+//     console.log(this.state.userName)
+
+//     socket.on('connect', () => {
+//       //1a
+//       socket.emit('join', { name: this.state.userName });
+
+//       socket.on('onlineUser', (payload) => {
+//         console.log("onlineUser", payload);
+//         console.log({ ...this.state.onlineUser })
+//         this.setState({ onlineUser: [...this.state.onlineUser, payload] });
+//       });
+//       socket.on('offlineUser', (payload) => {
+//         console.log("offlineUser", payload);
+
+//       });
+//     });
+//   }
+
+//   render() {
+
+
+//     return (
+//       <div>
+
+//       </div>
+//     )
+//   }
+// }
+
+import React from 'react';
+
+
+class Admin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      staffName: '',
+      tickets: [],
+      onlineStaff: [],
+    };
+
+  }
+  componentDidMount() {
+    // run once when component is mounted
+    const staffName = prompt("WHAT's your name?");
+    this.setState({ staffName });
+    console.log("sososo", this.props.socket);
+    this.props.socket.on('connect', () => {
+      //1a
+      this.props.socket.emit('join', { name: staffName });
+      this.props.socket.emit('getAll');
+      this.props.socket.on('newTicket', (payload) => {
+        this.setState({ tickets: [...this.state.tickets, payload] });
+      });
+      this.props.socket.on('onlineStaff', (payload) => {
+        this.setState({ onlineStaff: [...this.state.onlineStaff, payload] });
+      });
+      this.props.socket.on('offlineStaff', (payload) => {
+        console.log('HELLO?', payload);
+        this.setState({
+          onlineStaff: this.state.onlineStaff.filter((staff) => staff.id !== payload.id),
+        });
+      });
+    });
+  }
+  handleClaim = (id, socketId) => {
+    console.log(socketId);
+    this.props.socket.emit('claim', {
+      id,
+      name: this.state.staffName,
+      studentId: socketId,
+    });
+  };
   render() {
     return (
-      <div>
-        from user
-      </div>
-    )
+      <main className="admin-container">
+        <section id="container">
+          <h2>Opened Tickets</h2>
+          <section id="tickets">
+            {this.state.tickets.map((ticket) => {
+              return (
+                <div >hiiii</div>
+              );
+            })}
+          </section>
+        </section>
+        <aside id="online-staff">
+          <h2>Available TAs</h2>
+          {this.state.onlineStaff.map((staff) => (
+            <h2 key={staff.id}>{staff.name}</h2>
+          ))}
+        </aside>
+      </main>
+    );
   }
 }
+
+export default Admin;
